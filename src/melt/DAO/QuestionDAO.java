8004b4b7edb110528f7af4470c6e5b9dbec72d53
Ryzal Yusoff <ryzalyusoff@gmail.com>
2014-10-08 12:52:49 +0100

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	src/melt/Main.java
    	src/melt/Question.java
    	src/melt/QuestionDAO.java

---
+++ src/melt/DAO/QuestionDAO.java
@@ -0,0 +1,102 @@
+package melt.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import melt.Question;
+
+public class QuestionDAO {
+ 
+    private Connection con;
+    private Statement st;
+    private ResultSet rs;
+    
+    public QuestionDAO(){
+         connectDb();
+    }
+    
+    // Make connection to database
+    public void connectDb() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/melt","root","");
+            st = con.createStatement();
+            
+        } catch(Exception ex) {
+            System.out.println("Erro: "+ex);
+        }
+    }
+    
+    // Get all Question
+    public List<Question> getAllQuestion() throws Exception {
+    
+        List<Question> list = new ArrayList<>();
+        
+        Statement myStmt = null;
+        ResultSet myRs = null;
+       
+        Statement myQtmt = null;
+        ResultSet myQs = null;
+        
+        try {
+            myStmt = con.createStatement();
+            myRs = st.executeQuery("SELECT * FROM questions");
+            
+        
+            int count = 1;
+            while (myRs.next()) {
+                Question tempQuestion = convertRowToQuestion(myRs, count);
+                list.add(tempQuestion);
+                
+                count++;
+            }
+            return list;
+        
+        } finally {
+            close(myStmt, myRs);
+        }    
+    }
+    
+    private Question convertRowToQuestion(ResultSet myRs, int counter) throws SQLException {
+
+            int id = myRs.getInt("id");
+            //int id = counter;
+            
+            String question = myRs.getString("question");
+            String answer = myRs.getString("answer");
+
+            Question tempQuestion = new Question(id, question, answer);
+
+            return tempQuestion;
+    }
+    
+    /* Closing db connecttion starts */
+    private void close(Statement myStmt, ResultSet myRs) throws SQLException {
+		close(null, myStmt, myRs);		
+    }
+    
+    private static void close(Connection myConn, Statement myStmt, ResultSet myRs)
+			throws SQLException {
+
+		if (myRs != null) {
+			myRs.close();
+		}
+
+		if (myStmt != null) {
+			
+		}
+		
+		if (myConn != null) {
+			myConn.close();
+		}
+    }
+    /* Closing db connecttion starts */
+    
+    
+    
+}
---
+++ src/melt/QuestionTableModel.java
@@ -0,0 +1,56 @@
+package melt;
+
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+public class QuestionTableModel extends AbstractTableModel {
+
+    private static final int NO_COL = 0;
+    private static final int QUESTION_COL = 1;
+    private static final int ANSWER_COL = 2;
+    
+    private String[] columnNames = { "No", "Question"};
+    private List<Question> questions;
+    
+    public QuestionTableModel(List<Question> theQuestions) {
+		questions = theQuestions;
+    }
+    
+    @Override
+	public int getColumnCount() {
+		return columnNames.length;
+	}
+
+	@Override
+	public int getRowCount() {
+		return questions.size();
+	}
+
+	@Override
+	public String getColumnName(int col) {
+		return columnNames[col];
+	}
+
+	@Override
+	public Object getValueAt(int row, int col) {
+
+		Question tempQuestion = questions.get(row);
+
+		switch (col) {
+                case NO_COL:
+			return tempQuestion.getId();    
+		case QUESTION_COL:
+			return tempQuestion.getQuestion();
+		//case ANSWER_COL:
+		//	return tempQuestion.getAnswer();
+		default:
+			return tempQuestion.getQuestion();
+		}
+	}
+
+	@Override
+	public Class getColumnClass(int c) {
+		return getValueAt(0, c).getClass();
+	}
+    
+}
