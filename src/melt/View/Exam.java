6ff420fb358265b7422fdf29b1aa9d391a2fe184
Aote Zhou <aotechou@gmail.com>
2014-10-09 01:56:44 +0100

    modify

--- src/melt/View/Exam.java
+++ src/melt/View/Exam.java
@@ -5,9 +5,15 @@
  */
 package melt.View;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.ParseException;
@@ -16,7 +22,11 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.*;
+import javax.swing.border.Border;
+import melt.DAO.MCQ_DAO;
+import melt.DAO.Question_DAO;
 import melt.DAO.Section_DAO;
+import melt.DAO.Subsection_DAO;
 import melt.Model.*;
 
 /**
@@ -24,42 +34,108 @@ import melt.Model.*;
  * @author Aote Zhou
  */
 
-public class Exam extends JFrame implements ActionListener{
+public class Exam extends JFrame implements ActionListener,WindowListener{
 
     public JLabel welcomeLabel, timeLabel;
     public JButton addSectionButton;
     public ArrayList<Section> sections;
     public JLabel[] sectionLabels;
     public JButton[] buttons,deleteButtons;
+    public JPanel contentPanel;
+    public JPanel p1, p2, p3, p4,p5;
+    public JScrollPane jspane;
+    public int exam_ID;
 
-    public Exam() {
+    
+    public Exam(int exam_ID) {
+        this.setLocationRelativeTo(null);  //make window in the center of desktop
         setTitle("MELTSystem--Test");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 400);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        setSize(1000, 600);
         setContentPane(getGUI());
+        JMenuBar jMenuBar1=new JMenuBar();
+        JMenu jMenu1=new JMenu("ddd");
+        jMenuBar1.add(jMenu1);
+        getRootPane().setMenuBar(jMenuBar1);
+        this.exam_ID=exam_ID;
+        addWindowListener(this);
     }
 
     public JPanel getGUI() {
-        JPanel p1, p2, p3, p4;
+        
 
         welcomeLabel = new JLabel("....,Welcome!");
         timeLabel = new JLabel("dd-mm-yyyy hh:mm");
 
-        getSections(1);
+        
+        // create p1
+        p1 = new JPanel();
+        p1.setLayout(new GridLayout(1, 3));
+        p1.add(welcomeLabel);
+        p1.add(new JPanel());
+        p1.add(timeLabel);
+
+        //create p2
+        p2 = new JPanel();
+        setP2();
+        //Create p3
+        p3 = new JPanel();
+        
+        addSectionButton = new JButton("Add a Subsection");
+        addSectionButton.addActionListener(this);
+        
+        p3.setLayout(new BorderLayout());
+        //p3.add(new JLabel("Overall   6.0/30.0"));
+        p3.add(addSectionButton,BorderLayout.SOUTH);
+
+        //Create p4
+        p4 = new JPanel();
+        p4.setMaximumSize(new Dimension(100,1000));
+        p4.setLayout(new BoxLayout(p4, BoxLayout.Y_AXIS));
+        //p4.add(p1);
+        jspane=new JScrollPane(p2);
+        jspane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        p4.add(jspane);
+        p4.add(p3);
+        
+        //create p5
+        p5=new JPanel();
+        p5.setLayout(new BoxLayout(p5, BoxLayout.X_AXIS));
+        p5.add(p4);
+        contentPanel=new JPanel();
+        //contentPanel.setBackground(Color.red);
+        contentPanel.setLayout(new BorderLayout());
+        p5.add(contentPanel);
+        
+        
+        p2.setBackground(new Color(153, 153, 153));
+        p3.setBackground(new Color(153, 153, 153));
+        p4.setBackground(new Color(153, 153, 153));
+        
+        p4.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createTitledBorder(""), "Exam 1"));
+
+        return p5;
+    }
+
+    private void setP2(){
+        
+        
+        
+        getSections(exam_ID);
         
         buttons = new JButton[sections.size()];
         deleteButtons = new JButton[sections.size()];
         
-        addSectionButton = new JButton("Add Section");
-        addSectionButton.addActionListener(this);
+        
         
 
         sectionLabels=new JLabel[sections.size()];
+
         for (int i = 1; i < sections.size() + 1; i++) {
             Section section=sections.get(i-1);
             String sectionTimeLimit=new SimpleDateFormat("HH:mm:ss").format(section.getTimeLimit());
             
-            sectionLabels[i - 1] = new JLabel("Section" + i + "  " + section.getSection_Name()+"  time:"+sectionTimeLimit);
+            sectionLabels[i - 1] = new JLabel("Section" + i + "  " + section.getSection_Name());
             //Edit Button
             buttons[i - 1] = new JButton("Edit");
             buttons[i-1].setName(section.getSection_ID()+"");
@@ -72,15 +148,7 @@ public class Exam extends JFrame implements ActionListener{
             
 
         }
-        // create p1
-        p1 = new JPanel();
-        p1.setLayout(new GridLayout(1, 3));
-        p1.add(welcomeLabel);
-        p1.add(new JPanel());
-        p1.add(timeLabel);
-
-        //create p2
-        p2 = new JPanel();
+        
         //p2.setLayout(new GridLayout(sections.size(),3));
         GroupLayout groupLayout;
         GroupLayout.ParallelGroup horizontalGroup_P;
@@ -113,24 +181,10 @@ public class Exam extends JFrame implements ActionListener{
         groupLayout.setVerticalGroup(verticalGroup_S);
         
         p2.setLayout(groupLayout);
+        p2.revalidate();
+        //p2.repaint();
 
-        //Create p3
-        p3 = new JPanel();
-        //p3.add(new JLabel("Overall   6.0/30.0"));
-        p3.add(addSectionButton);
-
-        //Create p4
-        p4 = new JPanel();
-        p4.setLayout(new BoxLayout(p4, BoxLayout.Y_AXIS));
-        //p4.add(p1);
-        JScrollPane jspane=new JScrollPane(p2);
-        jspane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        p4.add(jspane);
-        p4.add(p3);
-
-        return p4;
     }
-
     private void getSections(int exam_ID) {
         try {
             Section_DAO section_DAO = new Section_DAO();
@@ -158,29 +212,94 @@ public class Exam extends JFrame implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource()==addSectionButton) {
-            this.dispose();
-            SettingSection settingSection=new SettingSection();
-            settingSection.setVisible(true);
+            //this.dispose();
+            SettingSection settingSection=new SettingSection(exam_ID);
+            //settingSection.setVisible(true);
+            contentPanel.removeAll();
+            JPanel temp=settingSection.getGUI();
+            
+            contentPanel.setLayout(new FlowLayout());
+            contentPanel.add(temp);
+            contentPanel.revalidate();
+            //contentPanel.repaint();
             
         }else if (((JButton)e.getSource()).getText().equals("Delete")) {
             // wait for complete
+            int section_ID=Integer.parseInt(((JButton)e.getSource()).getName());
+            MCQ_DAO mcq_DAO=new MCQ_DAO();
+            Question_DAO question_DAO=new Question_DAO();
+            Subsection_DAO subsection_DAO=new Subsection_DAO();
+            Section_DAO section_DAO=new Section_DAO();
+        
             
-        }else{  //Edit Button
-            this.dispose();
+            mcq_DAO.cancelRWithSubSec(section_ID);
+            question_DAO.cancelRWithSubSec(section_ID);
+            subsection_DAO.delete("Section_ID='"+section_ID+"'");
+            section_DAO.delete("Section_ID='"+section_ID+"'");
+            
+            p2.removeAll();
+            
+            setP2();
+            
+            
+            
+        }else if(((JButton)e.getSource()).getText().equals("Edit")){  //Edit Button
+           // this.dispose();
             int section_ID=Integer.parseInt(((JButton)e.getSource()).getName());
             SettingExam settingExam=new SettingExam(section_ID);
-            settingExam.setVisible(true);
+            contentPanel.removeAll();
+            contentPanel.setLayout(new BorderLayout());
+            contentPanel.add(settingExam.getGUI());
+            contentPanel.revalidate();
+            //contentPanel.repaint();
+            //settingExam.setVisible(true);
             
         }
     }
     public static void main(String[] args) {
-        Exam test = new Exam();
-        test.setTitle("MELTSystem--Test");
-        test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        test.setSize(400, 400);
-        test.setContentPane(test.getGUI());
+        Exam test = new Exam(1);
+        
         test.setVisible(true);
 
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        this.dispose();
+        ExamOverview examOverview=new ExamOverview();
+        examOverview.setVisible(true);
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+        
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
